957356c4a41b7018858a35b5a0a69a1f8612086b
Michael Carr <xyeres@me.com>
2021-08-31 18:17:51 -0700

    Adds working users menu

---
+++ main.js
@@ -0,0 +1,16 @@
+const menuIcon = document.querySelector('.infoBar__userMenuToggle');
+const screenContainer = document.querySelector('.screenContainer');
+const userMenu = document.querySelector('.userMenu');
+const messages = document.querySelector('.messages');
+
+const toggleMenu = () => {
+  screenContainer.classList.toggle('userMenu__activeMenu');
+  userMenu.classList.toggle('opanicty-none')
+}
+
+menuIcon.onclick = () => toggleMenu();
+messages.onclick = () => {
+  if (screenContainer.classList.contains('userMenu__activeMenu')) {
+    toggleMenu();
+  }
+}
